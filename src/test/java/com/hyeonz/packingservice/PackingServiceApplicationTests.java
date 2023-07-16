3017f9576b1742c251d38e2ed07ee78c5ce0232d
hyeon-z <phj8221@naver.com>
2023-07-17 07:46:11 +0900

    feat: 프로젝트 세팅

---
+++ src/test/java/com/hyeonz/packingservice/PackingServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.hyeonz.packingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackingServiceApplicationTests {
+
+}
