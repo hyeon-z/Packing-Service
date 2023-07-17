5c15b6a90ed95df53a19cf17223ca21f95719464
hyeon-z <phj8221@naver.com>
2023-07-17 14:08:54 +0900

    feat: Pack, PackingList 엔티티 생성자 구조 변경

--- src/main/java/com/hyeonz/packingservice/model/Pack.java
+++ src/main/java/com/hyeonz/packingservice/model/Pack.java
@@ -3,7 +3,7 @@ package com.hyeonz.packingservice.model;
 import java.time.LocalDateTime;
 
 public class Pack {
-    private final long id;
+    private long id;
     private final long packingListId;
     private final String name;
     private final Category category;
@@ -11,8 +11,7 @@ public class Pack {
     private final LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public Pack(long id, long packingListId, String name, Category category) {
-        this.id = id;
+    public Pack(long packingListId, String name, Category category) {
         this.packingListId = packingListId;
         this.name = name;
         this.category = category;
--- src/main/java/com/hyeonz/packingservice/model/PackingList.java
+++ src/main/java/com/hyeonz/packingservice/model/PackingList.java
@@ -5,30 +5,19 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class PackingList {
-    private final long id;
+    private long id;
     private final String title;
     private String description;
     private final LocalDate departureDate;
-    private final List<Pack> packs;
+    private List<Pack> packs;
 
     private final LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public PackingList(long id, String title, String description, LocalDate departureDate, List<Pack> packs) {
-        this.id = id;
+    public PackingList(String title, String description, LocalDate departureDate) {
         this.title = title;
         this.description = description;
         this.departureDate = departureDate;
-        this.packs = packs;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
-
-    public PackingList(long id, String title, LocalDate departureDate, List<Pack> packs) {
-        this.id = id;
-        this.title = title;
-        this.departureDate = departureDate;
-        this.packs = packs;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
