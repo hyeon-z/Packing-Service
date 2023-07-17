849f27ead52ce06e34ed839f0b91a278bff9dec8
hyeon-z <phj8221@naver.com>
2023-07-17 21:16:01 +0900

    refactor: schema.sql을 h2문법으로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE packing_list
 (
-    id             BIGINT PRIMARY KEY AUTO_INCREMENT,
-    title          VARCHAR(12) NOT NULL,
-    description    VARCHAR(30)          DEFAULT NULL,
-    departure_date DATE        NOT NULL,
-    created_at     DATETIME    NOT NULL DEFAULT NOW(),
-    updated_at     DATETIME    NOT NULL DEFAULT NOW()
-)
+    id             BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title          VARCHAR(12)        NOT NULL,
+    description    VARCHAR(30)                 DEFAULT NULL,
+    departure_date DATE               NOT NULL,
+    created_at     DATETIME           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     DATETIME           NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
