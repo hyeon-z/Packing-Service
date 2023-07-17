a667ccb8bfe979d15fbf657a06efdef6aa6a839d
hyeon-z <phj8221@naver.com>
2023-07-18 00:28:40 +0900

    feat: PackJdbcRepository의 insert, findAll메서드 구현

--- src/main/java/com/hyeonz/packingservice/repository/PackJdbcRepository.java
+++ src/main/java/com/hyeonz/packingservice/repository/PackJdbcRepository.java
@@ -2,13 +2,33 @@ package com.hyeonz.packingservice.repository;
 
 import com.hyeonz.packingservice.model.Category;
 import com.hyeonz.packingservice.model.Pack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Repository
 public class PackJdbcRepository implements PackRepository {
-    @Override
-    public Pack insert(Pack pack) {
-        return null;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    private final RowMapper<Pack> packRowMapper = (rs, rowNum) -> new Pack(
+            rs.getLong("id"),
+            rs.getLong("packing_list_id"),
+            rs.getString("name"),
+            Category.valueOf(rs.getString("category")),
+            rs.getBoolean("checked"),
+            rs.getTimestamp("created_at").toLocalDateTime(),
+            rs.getTimestamp("updated_at").toLocalDateTime()
+    );
+
+    public PackJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
@@ -22,7 +42,35 @@ public class PackJdbcRepository implements PackRepository {
     }
 
     @Override
-    public void delete() {
+    public void deleteById(long id) {
 
+    }
+
+    @Override
+    public List<Pack> findAll() {
+        return jdbcTemplate.query("SELECT * FROM pack", packRowMapper);
+    }
+
+    @Override
+    public Pack insert(Pack pack) {
+        int update = jdbcTemplate.update("INSERT INTO pack(packing_list_id, name, category) "
+                + "VALUES (:packingListId, :name, :category)", toParamMap(pack)
+        );
+
+        if (update != 1) {
+            logger.error("Packi의 insert가 제대로 되지 않았습니다.");
+            throw new RuntimeException("Pack의 insert가 제대로 되지 않았습니다.");
+        }
+        return pack;
+    }
+
+    private Map<String, Object> toParamMap(Pack pack) {
+        HashMap<String, Object> paramMap = new HashMap<>();
+
+        paramMap.put("packingListId", pack.getPackingListId());
+        paramMap.put("name", pack.getName());
+        paramMap.put("category", pack.getCategory().toString());
+
+        return paramMap;
     }
 }
