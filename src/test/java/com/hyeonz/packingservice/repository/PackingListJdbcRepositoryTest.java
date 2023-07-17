72a17f616c3c95b4c6e602e64a371f7303a05280
hyeon-z <phj8221@naver.com>
2023-07-17 21:24:10 +0900

    test: embeddedDatabase MySQL에서 H2로 변경

--- src/test/java/com/hyeonz/packingservice/repository/PackingListJdbcRepositoryTest.java
+++ src/test/java/com/hyeonz/packingservice/repository/PackingListJdbcRepositoryTest.java
@@ -1,48 +1,40 @@
 package com.hyeonz.packingservice.repository;
 
 import com.hyeonz.packingservice.model.PackingList;
-import com.wix.mysql.EmbeddedMysql;
-import com.wix.mysql.ScriptResolver;
-import com.wix.mysql.config.Charset;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
-import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
-import static com.wix.mysql.distribution.Version.v5_7_10;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalToObject;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@ActiveProfiles("test")
 class PackingListJdbcRepositoryTest {
 
-    static EmbeddedMysql embeddedMysql;
+    static EmbeddedDatabase embeddedDatabase;
 
     @BeforeAll
     static void setup() {
-        var config = aMysqldConfig(v5_7_10)
-                .withCharset(Charset.UTF8)
-                .withPort(2215)
-                .withUser("test", "test1234!")
-                .withTimeZone("Asia/Seoul")
+        embeddedDatabase = new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript("schema.sql")
                 .build();
-        embeddedMysql = anEmbeddedMysql(config)
-                .addSchema("test-packing_service", ScriptResolver.classPathScript("schema.sql"))
-                .start();
     }
 
     @AfterAll
     static void cleanup() {
-        embeddedMysql.stop();
+        embeddedDatabase.shutdown();
     }
 
     @Autowired
@@ -59,19 +51,19 @@ class PackingListJdbcRepositoryTest {
         List<PackingList> packingLists = repository.findAll();
 
         assertThat(packingLists.isEmpty(), is(false));
-        assertThat(insertList, equalToObject(packingList));
+        assertThat(insertList, samePropertyValuesAs(packingList));
     }
 
     @Test
     @Order(2)
     @DisplayName("null 정보를 넣은 패킹리스트를 추가할 수 있다.")
     void insertNullInfoPackingList() {
-        PackingList packingList = new PackingList("대만여행", null, LocalDate.now());
-        PackingList insertList = repository.insert(packingList);
+        PackingList nullInfoList = new PackingList("대만여행", null, LocalDate.now());
+        PackingList insertList = repository.insert(nullInfoList);
 
         List<PackingList> packingLists = repository.findAll();
 
         assertThat(packingLists.isEmpty(), is(false));
-        assertThat(insertList, equalToObject(packingList));
+        assertThat(insertList, samePropertyValuesAs(nullInfoList));
     }
 }
