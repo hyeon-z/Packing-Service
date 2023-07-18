32957631994f2eab7bfbde0dddf5d6b275d702b4
hyeon-z <phj8221@naver.com>
2023-07-18 11:52:39 +0900

    feat: Pack 테이블 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,15 @@ CREATE TABLE packing_list
     created_at     DATETIME           NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at     DATETIME           NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE pack
+(
+    id              BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    packing_list_id BIGINT,
+    name            VARCHAR(12)        NOT NULL,
+    category        VARCHAR(12)        NOT NULL,
+    checked         BOOLEAN                     DEFAULT FALSE,
+    created_at      DATETIME           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      DATETIME           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (packing_list_id) REFERENCES packing_list (id)
+);
